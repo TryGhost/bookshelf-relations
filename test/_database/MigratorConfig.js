24883b8b2dc541c12da4cd05845c9f955ec1f217
Sam Lord <hello@samlord.me>
2026-07-08 12:52:44 +0100

    Repair CI after Renovate updates
    # Conflicts:
    #	package.json
    
    # Conflicts:
    #	pnpm-lock.yaml
    
    # Conflicts:
    #	pnpm-lock.yaml
    
    # Conflicts:
    #	pnpm-lock.yaml

--- test/_database/MigratorConfig.js
+++ test/_database/MigratorConfig.js
@@ -1,8 +1,12 @@
 const config = require('../../config');
 const path = require('path');
 
+const getDatabaseConfig = function getDatabaseConfig() {
+    return JSON.parse(JSON.stringify(config.get('database')));
+};
+
 module.exports = {
     currentVersion: '1.0',
-    database: config.get('database'),
+    database: getDatabaseConfig(),
     migrationPath: path.join(__dirname, 'migrations')
 };
